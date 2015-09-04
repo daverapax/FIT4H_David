cc790af88f0db6cd27e924d96ec8615854caf218
daverapax <generation.doof@googlemail.com>
2015-09-04 11:41:35 +0200

    Update Ruby

---
+++ ruby/code/studio_game.rb
@@ -0,0 +1,10 @@
+name1 = "larry"
+health = 60
+puts name1 + "s health is #{health}"
+puts "#{name1}s health is #{3*health}\n"
+puts "#{name1}s health is #{health/9}\n"
+puts "#{name1}s health is #{health/9.0}\n"
+puts "Players:\n\tlarry\n\tcurly\n\tmoe\n\n"
+name2 = "curly"
+name3 = "moe"
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}\n\n"
